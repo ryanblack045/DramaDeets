6cb73b3f170d153fdb84abba2a44aa48f5239557
Ryan Black <ryanblack045@gmail.com>
2021-01-03 12:00:31 -0500

    about creator finished

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -5,8 +5,12 @@ import {
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  aboutMe: {
+    fontSize: "1.5em",
+    fontFamily: "gt-super, Georgia, Cambria, Times New Roman, Times, serif;",
+    margin: "1em 1em -.5em 1em"
+  },
   drawer: {
-
   },
   drawerButton: {
     marginLeft: "1.5em",
@@ -42,8 +46,33 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "space-between"
   },
+  gitHub: {
+    heigh: "2.6em",
+    width: "2.6em",
+  },
+  icons: {
+    display: "flex",
+    alignItems: "center"
+  },
+  infoContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  linkedIn: {
+    heigh: "2em",
+    width: "2em",
+    marginRight: ".5em",
+  },
   myInfo: {
     height: "40vh"
+  },
+  profilePic: {
+    justifyContent: "center",
+    height: "14em",
+    width: "14em",
+    borderRadius: "14em",
+    padding: "1em"
   }
 }))
 
@@ -73,8 +102,32 @@ export default function Footer() {
           className={classes.drawer}
           open={drawerOpen}
           onClose={toggleDrawer}>
-          <div>
-            <div className={classes.myInfo}>Hello World</div>
+          <div className={classes.infoContainer}>
+          <img
+            className={classes.profilePic}
+            src="https://drama-deets.s3.amazonaws.com/ryanprofile.jpeg"
+            alt="Profile Pic" />
+            <span className={classes.aboutMe}>Hi! My name is Ryan, I'm an artist/software engineer based in NYC. I hope that you find this site is useful, if you have any ideas or suggestions I'd be happy to hear from you. Also my github and linkedIn pages are listed below.
+              <br></br>
+              <span className={classes.icons}>
+              <a
+                href="https://www.linkedin.com/in/ryanblack045/"
+                className={classes.aTag}>
+                <img
+                  className={classes.linkedIn}
+                  src="https://drama-deets.s3.amazonaws.com/linkedin.svg"
+                  alt="LinkedIn icon"/>
+              </a>
+              <a
+                href="https://github.com/ryanblack045"
+                className={classes.aTag}>
+                <img
+                  className={classes.gitHub}
+                  src="https://drama-deets.s3.amazonaws.com/githubIcon.png"
+                  alt="Github icon"/>
+                </a>
+                </span>
+            </span>
           </div>
           </Drawer>
       </div>
