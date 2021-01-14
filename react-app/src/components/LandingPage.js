4d1acf75e4b60a9f37bc1c3c08c534b88150f42c
Ryan Black <ryanblack045@gmail.com>
2021-01-14 14:15:32 -0500

    edited landing page for admin account

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AOS from 'aos';
+import { useSelector} from "react-redux";
 import 'aos/dist/aos.css';
 
 
@@ -9,7 +10,13 @@ const useStyles = makeStyles((theme) => ({
     backgroundImage: 'url("https://drama-deets.s3.amazonaws.com/landingPage1.png")',
     height: "100vh",
     marginLeft: "5%",
-    // marginRight: "5%",
+    width: "auto",
+    backgroundRepeat: "no-repeat"
+  },
+  img2: {
+    backgroundImage: 'url("https://drama-deets.s3.amazonaws.com/adminLanding.png")',
+    height: "100vh",
+    marginLeft: "5%",
     width: "auto",
     backgroundRepeat: "no-repeat"
 }
@@ -20,15 +27,22 @@ const useStyles = makeStyles((theme) => ({
 export default function LandingPage() {
   AOS.init()
   const classes = useStyles();
-
+  const currentUserId = useSelector((state) => (state.session.currentUser.id))
   return (
     <>
-      <div
-        data-aos="fade-up"
-        data-aos-delay="50"
-        data-aos-duration="1000"
-        className={classes.img1}
-      />
+      {currentUserId !== 1 ?
+        <div
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          className={classes.img1}
+        />
+      : <div
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          className={classes.img2}
+        />}
     </>
   )
 
