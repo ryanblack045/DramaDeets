9707aad0d85c523f91f9f3e2b57318a9dd065f27
Ryan Black <ryanblack045@gmail.com>
2021-04-19 08:27:42 -0400

    cleaning up

--- app/__init__.py
+++ app/__init__.py
@@ -41,12 +41,6 @@ Migrate(app, db)
 # Application Security
 CORS(app)
 
-# @app.before_request
-# def redirect_https():
-#   if request.headers.get('X-Forwarded-Proto') == 'http':
-#     url = request.url.replace('http://', 'https://', 1)
-#     code = 301
-#     return redirect(url, code=code)
 
 @app.after_request
 def inject_csrf_token(response):
--- react-app/src/components/CurrentBusinessDisplay.js
+++ react-app/src/components/CurrentBusinessDisplay.js
@@ -17,20 +17,20 @@ import BusinessStyles from '../styles/BusinessStyles'
   return (
     <>
     <div className={classes.businessParentContainer}>
-    <img className={classes.businessImg} src={currentBusiness.imgURL} alt="Headshot of actress" />
-  <div className={classes.businessInfoContainer}>
-  <div className={classes.businessTitle}>{currentBusiness.name}</div>
-  <div className={classes.businessCSZ}>{currentBusiness.address}</div>
-  <div className={classes.businessCSZ}>
-    {currentBusiness.city}, {currentBusiness.state} {currentBusiness.zipcode}
-  </div>
-  <div className={classes.businessCSZ}>
-    <a href={currentBusiness.website}>{currentBusiness.website}</a>
-  </div>
-  <div className={classes.businessCSZ}>Contact: {currentBusiness.contact}</div>
-        <div className={classes.businessRating}>Rating: {!ratingCalculator(currentBusiness) ? "No Reviews" : ratingCalculator(currentBusiness).toFixed(1) + `/10.0`}</div>
+      <img className={classes.businessImg} src={currentBusiness.imgURL} alt="Headshot of actress" />
+      <div className={classes.businessInfoContainer}>
+        <div className={classes.businessTitle}>{currentBusiness.name}</div>
+        <div className={classes.businessCSZ}>{currentBusiness.address}</div>
+        <div className={classes.businessCSZ}>
+          {currentBusiness.city}, {currentBusiness.state} {currentBusiness.zipcode}
         </div>
-  </div>
+        <div className={classes.businessCSZ}>
+          <a href={currentBusiness.website}>{currentBusiness.website}</a>
+        </div>
+        <div className={classes.businessCSZ}>Contact: {currentBusiness.contact}</div>
+        <div className={classes.businessRating}>Rating: {!ratingCalculator(currentBusiness) ? "No Reviews" : ratingCalculator(currentBusiness).toFixed(1) + `/10.0`}</div>
+      </div>
+    </div>
   </>
   )
 }
